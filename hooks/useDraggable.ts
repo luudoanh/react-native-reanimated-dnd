dd85ebdee37f3031032d31a25f5f20fe3028e063
root <root@DESKTOP-58JMULF.localdomain>
2025-10-16 14:41:45 +0700

    Fix TypeScript errors and build package for npm publishing

--- hooks/useDraggable.ts
+++ hooks/useDraggable.ts
@@ -179,9 +179,9 @@ export const useDraggable = <TData = unknown>(
         }
 
         // Check children recursively
-        if (child.props && child.props.children) {
+        if (child.props && (child.props as any).children) {
           if (
-            React.Children.toArray(child.props.children).some(checkForHandle)
+            React.Children.toArray((child.props as any).children).some(checkForHandle)
           ) {
             return true;
           }
